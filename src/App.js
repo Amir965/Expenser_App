16fff209ea05234b2ed55061fdec4aaec0d71534
Amir965 <sohailaamir965@gmail.com>
2022-06-02 16:33:48 +0530

    location update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddExpense from "./pages/add-expense";
-
+import React,{useEffect} from "react";
 const App = () => {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  });
   return (
     <BrowserRouter>
       <Header />
